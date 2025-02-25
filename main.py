9dda35908080f3addc3ef093879032391ee23231
ARISHA <164086149+Arishayy@users.noreply.github.com>
2025-02-25 23:07:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+# assignment 1
+# Exploring DataTypes in Python
+
+
+# integer
+a = 10
+print ("The value of a is", a)
+print ("Its data type is ", type(a))
+
+print("\n")
+
+# float
+b = 10.5
+print ("The value of b is", b)
+print ("Its data type is ", type(b))
+
+print("\n")
+
+# complex
+c = 3 + 4j
+print ("The value of c is", c)
+print ("Its data type is ", type(c))
+
+print("\n")
+
+# string
+d = "Hello Arisha"
+print ("The value of d is", d)
+print ("Its data type is ", type(d))
+
+print("\n")
+
+# boolean
+e = True
+print ("The value of e is", e)
+print ("Its data type is ", type(e))
+
+print("\n")
+
+# list
+f = [1, 2, 3, 4, 5]
+print ("The value of f is", f)
+print ("Its data type is ", type(f))
+
+print("\n")
+
+# tuple
+g = (1, 2, 3, 4, 5)
+print ("The value of g is", g)
+print ("Its data type is ", type(g))
+
+print("\n")
+
+# dictionary
+h = {"Fav Fruit" : "Apple", "Fav Color" : "Blue"}
+print ("The value of h is", h)
+print ("Its data type is ", type(h))
+
+print("\n")
+
+# set
+i = {1, 2, 3, 4, 5}
+print ("The value of i is", i)
+print ("Its data type is ", type(i))
+
+print("\n")
+     
+# tuple
+j = (1, 2, 3, 4, 5)
+print ("The value of j is", j)
+print ("Its data type is ", type(j))
+
+print("\n")
+
+# none
+k = None
+print ("The value of k is", k)
+print ("Its data type is ", type(k))
+
+print("\n")
+
+# frozenset
+l = frozenset({1, 2, 3, 4, 5})
+print ("The value of l is", l)
+print ("Its data type is ", type(l))
